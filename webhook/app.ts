face0d58177d492fd5edaabe8d8d12d30b4fdc2b
Davin <lampdavin@gmail.com>
2023-06-04 14:17:12 -0400

    Add a bunch of stuff

--- webhook/app.ts
+++ webhook/app.ts
@@ -1,4 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'; // ES Modules import
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 /**
  *
@@ -15,25 +18,69 @@ export const lambdaHandler = async (
 ): Promise<APIGatewayProxyResult> => {
   console.log(JSON.stringify(event));
 
+  let repoUuid = '';
+  let gitLink = '';
   if (event.body) {
-    const bodyParsed = JSON.parse(event.body);
-    console.log(JSON.stringify(bodyParsed));
-  }
-
-  try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
-    };
-  } catch (err) {
-    console.log(err);
+    console.log(JSON.parse(event.body));
+    repoUuid = JSON.parse(event.body).repository.name;
+    gitLink = JSON.parse(event.body).repository.url;
+  } else {
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        message: 'some error happened',
-      }),
+      body: '',
     };
   }
+
+  let wikiUrl = '';
+  if (process.env.WIKI_URL) {
+    wikiUrl = process.env.WIKI_URL;
+  } else {
+    return {
+      statusCode: 500,
+      body: '',
+    };
+  }
+
+  let s3Name = '';
+  if (process.env.WIKI_ARTIFACT_BUCKET_NAME) {
+    s3Name = process.env.WIKI_ARTIFACT_BUCKET_NAME;
+  } else {
+    return {
+      statusCode: 500,
+      body: '',
+    };
+  }
+
+  console.log(repoUuid);
+  console.log(JSON.stringify(process.env.WIKI_REPO_CREATED_TABLE_NAME));
+
+  const docClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+  const command = new PutCommand({
+    TableName: process.env.WIKI_REPO_CREATED_TABLE_NAME,
+    Item: {
+      id: repoUuid,
+      data: {
+        gitlink: gitLink,
+        permalink: wikiUrl + '/' + repoUuid,
+        titelink: '',
+      },
+    },
+  });
+  const response = await docClient.send(command);
+  console.log(JSON.stringify(response));
+
+  const s3Client = new S3Client({});
+  const putInput = {
+    Bucket: s3Name,
+    Key: repoUuid + '/',
+    ContentLength: 0,
+  };
+  const putCommand = new PutObjectCommand(putInput);
+  const putResponse = await s3Client.send(putCommand);
+  console.log(JSON.stringify(putResponse));
+
+  return {
+    statusCode: 200,
+    body: '',
+  };
 };
---
+++ wiki-create/app.ts
@@ -0,0 +1,109 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager';
+import { v4 as uuidv4 } from 'uuid';
+import { App } from 'octokit';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log(JSON.stringify(event));
+
+  const secretName = 'alareti-githubapp';
+
+  const client = new SecretsManagerClient({
+    region: 'us-east-2',
+  });
+
+  const secretResponse = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secretName,
+    }),
+  );
+
+  const secret = secretResponse.SecretString;
+  if (!secret)
+    return {
+      statusCode: 500,
+      body: '',
+    };
+
+  let body = '';
+  let statusCode = 0;
+  if (event.httpMethod === 'OPTIONS') {
+    statusCode = 201;
+    body = '';
+  } else {
+    statusCode = 200;
+    const app = new App({
+      appId: 339960,
+      privateKey: secret,
+    });
+
+    console.log('Here I am');
+    const octokit = await app.getInstallationOctokit(38017192);
+
+    const githubResponse = await octokit.rest.repos.createInOrg({
+      org: 'alareti',
+      name: uuidv4(),
+    });
+    body = JSON.stringify(githubResponse);
+    console.log(body);
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      'access-control-allow-headers':
+        'access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,content-type',
+      'Access-Control-Allow-Origin': '*',
+      'access-control-allow-methods': 'POST, OPTIONS',
+      'content-type': 'application/json',
+    },
+    body: body,
+  };
+
+  try {
+    return response;
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
+
+  // if (event.body) {
+  //   const bodyParsed = JSON.parse(event.body);
+  //   console.log(JSON.stringify(bodyParsed));
+  // }
+
+  // try {
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(event),
+  //   };
+  // } catch (err) {
+  //   console.log(err);
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       message: 'some error happened',
+  //     }),
+  //   };
+  // }
+};
