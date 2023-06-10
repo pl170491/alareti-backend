7abd5d9eb19d499efecaa933b2abf79e36da50cd
Davin Lampert <lampdavin@gmail.com>
2023-06-10 17:11:21 -0400

    404 functionality in wiki-article-read

--- wiki-article-read/app.ts
+++ wiki-article-read/app.ts
@@ -1,4 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
 
 /**
  *
@@ -13,25 +16,58 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  console.log(JSON.stringify(event));
+  // console.log(JSON.stringify(event));
 
-  if (event.body) {
-    const bodyParsed = JSON.parse(event.body);
-    console.log(JSON.stringify(bodyParsed));
-  }
-
-  try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(event),
-    };
-  } catch (err) {
-    console.log(err);
+  let wikiTableName = '';
+  if (process.env.WIKI_REPO_CREATED_TABLE_NAME) {
+    wikiTableName = process.env.WIKI_REPO_CREATED_TABLE_NAME;
+  } else {
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        message: 'some error happened',
-      }),
+      body: '',
     };
   }
+
+  const docClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+  const queryInput = {
+    TableName: wikiTableName,
+    KeyConditionExpression: 'id = :partitionKey',
+    ExpressionAttributeValues: {
+      ':partitionKey': 'fixMe',
+    },
+  };
+  const queryCommand = new QueryCommand(queryInput);
+  const queryResponse = await docClient.send(queryCommand);
+
+  if (queryResponse.Count != 1) {
+    return {
+      statusCode: 404,
+      body: '',
+    };
+  }
+
+  // let s3Name = '';
+  // if (process.env.WIKI_ARTIFACT_BUCKET_NAME) {
+  //   s3Name = process.env.WIKI_ARTIFACT_BUCKET_NAME;
+  // } else {
+  //   return {
+  //     statusCode: 500,
+  //     body: '',
+  //   };
+  // }
+  // console.log(s3Name);
+
+  // const s3Client = new S3Client({});
+  // const getObjectInput = {
+  //   Bucket: s3Name,
+  //   Key: 'iDontExist',
+  // };
+  // const getObjectCommand = new GetObjectCommand(getObjectInput);
+  // const response = await s3Client.send(getObjectCommand);
+  // console.log(response);
+
+  return {
+    statusCode: 200,
+    body: '',
+  };
 };
